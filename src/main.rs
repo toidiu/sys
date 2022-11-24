2a19322371d4d42f1673f4cb85d0f4d56fdba3d8
toidiu <apoorv@toidiu.com>
2022-11-24 02:22:41 -0800

    display cpu

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
+use std::fmt::Display;
 use std::process::Command;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 use structopt::StructOpt;
 use sysinfo::Pid;
 use sysinfo::PidExt;
-use sysinfo::{self, CpuExt, CpuRefreshKind, System, SystemExt};
+use sysinfo::{self, System, SystemExt};
 use sysinfo::{NetworkExt, ProcessExt};
 
 pub type Result<T, E = Error> = core::result::Result<T, E>;
@@ -59,8 +60,12 @@ impl Stats {
     fn collect(&mut self, is_running: Arc<AtomicBool>) {
         self.system.refresh_networks_list();
         loop {
-            self.get_cpu();
-            self.get_net();
+            let mut info = StatInfo::new(self.pid);
+            self.get_cpu(&mut info);
+            self.get_net(&mut info);
+
+            println!("{}", info);
+
             if !is_running.load(Ordering::Relaxed) {
                 return;
             }
@@ -69,7 +74,7 @@ impl Stats {
         }
     }
 
-    fn get_cpu(&mut self) {
+    fn get_cpu(&mut self, info: &mut StatInfo) {
         // Refreshing CPU information.
         // self.system
         // .refresh_cpu_specifics(CpuRefreshKind::new().with_cpu_usage());
@@ -78,24 +83,64 @@ impl Stats {
             .refresh_process_specifics(self.pid, sysinfo::ProcessRefreshKind::new().with_cpu());
 
         if let Some(p) = self.system.process(self.pid) {
-            let usage = p.cpu_usage();
-            println!("---------pid: {}, cpu: {}% ", self.pid, usage);
+            info.cpu = p.cpu_usage();
         } else {
             println!("----------cant find");
         }
     }
 
-    fn get_net(&mut self) {
+    fn get_net(&mut self, info: &mut StatInfo) {
         self.system.refresh_networks();
 
         for (interface_name, network) in self.system.networks() {
-            println!(
-                "{} rx: {} tx: {}",
-                interface_name,
+            let net = NetworkStatInfo::new(
+                interface_name.to_string(),
                 network.received(),
-                network.transmitted()
+                network.transmitted(),
             );
-            // println!("in: {} B", network.received());
+            info.net.push(net);
         }
+    }
+}
+
+#[derive(Debug)]
+struct StatInfo {
+    pid: sysinfo::Pid,
+    cpu: f32,
+    net: Vec<NetworkStatInfo>,
+}
+
+impl StatInfo {
+    fn new(pid: sysinfo::Pid) -> Self {
+        StatInfo {
+            pid,
+            cpu: 0.0,
+            net: Vec::new(),
+        }
+    }
+}
+
+impl Display for StatInfo {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("{}, ", &self.pid))?;
+        f.write_fmt(format_args!("{}| ", &self.cpu))?;
+        for net in &self.net {
+            f.write_fmt(format_args!("{} {} {}| ", net.name, net.rx, net.tx))?;
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+struct NetworkStatInfo {
+    name: String,
+    rx: u64,
+    tx: u64,
+}
+
+impl NetworkStatInfo {
+    fn new(name: String, rx: u64, tx: u64) -> Self {
+        Self { name, rx, tx }
     }
 }
